af625c36fe2dd4799271c43b6c3b1f15841bacc9
ChiangYintso <392711804@qq.com>
2020-11-15 13:41:51 +0800

    bug fix

--- setup.py
+++ setup.py
@@ -4,13 +4,6 @@ from setuptools import setup, find_packages
 
 from py_commons import __version__
 
-
-def _get_install_requires():
-    with open("requirements.txt") as f:
-        lines = f.readlines()
-        return list(map(lambda s: s.strip(), lines))
-
-
 name = "py_commons"
 setup(
     name=name,
@@ -19,6 +12,5 @@ setup(
     description="Common utils for python",
     packages=find_packages(include=[name, f"{name}.*"], exclude=["test", "test.*"]),
     include_package_data=True,
-    platforms="any",
-    install_requires=_get_install_requires()
+    platforms="any"
 )
